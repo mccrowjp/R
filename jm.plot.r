1ba5f257cdb3adf9b853a58f7aa57ec3cb43401a
mccrowjp <john.mccrow@gmail.com>
2016-04-30 20:02:17 -0700

    plotxy_marginhists

--- jm.plot.r
+++ jm.plot.r
@@ -22,6 +22,36 @@ colored_column = function(x, mark=rep(FALSE,length(x)), minx=min(x), maxx=max(x)
     }
 }
 
+# Scatter plot with marginal histograms
+plotxy_marginhists = function(x, y, margins=c(5,5), border=1, h.axis=TRUE, h1.axis=h.axis, h2.axis=h.axis, h.b=20, h1.b=h.b, h2.b=h.b, h.col=8, h1.col=h.col, h2.col=h.col, log="", ...) {
+    lmat = matrix(c(2,4,1,3), ncol=2)
+    layout(lmat, widths=c(4/5, 1/5), heights=c(1/5, 4/5))
+    
+    plot(1:1,axes=F,type="n",xlab="",ylab="")
+    
+    df = data.frame(x,y)
+    if(grepl("x", log)) { df = df[df[,1]>0,] }
+    if(grepl("y", log)) { df = df[df[,2]>0,] }
+    x = df[,1]
+    y = df[,2]
+    
+    hx = hist(x, plot=F, breaks=h1.b)
+    hy = hist(y, plot=F, breaks=h2.b)
+    if(grepl("x", log)) { hx = hist(log(x), plot=F, breaks=h1.b) }
+    if(grepl("y", log)) { hy = hist(log(y), plot=F, breaks=h2.b) }
+    
+    par(mar=c(0,margins[2],1,1))
+    barplot(hx$counts, axes=F, ylim=c(0, max(hx$counts)), space=0, col=h1.col, border=border)
+    if(h1.axis) { axis(2) }
+    
+    par(mar=c(margins[1],0,1,1))
+    barplot(hy$counts, axes=F, xlim=c(0, max(hy$counts)), space=0, col=h2.col, border=border, horiz=TRUE)
+    if(h2.axis) { axis(1) }
+    
+    par(mar=c(margins[1],margins[2],1,1))
+    plot(x, y, log=log, ...)
+}
+
 # MA-plot
 plot_MA=function(x, y, sig=rep(FALSE,length(x)), ...) {
     x[x<1]=NA
